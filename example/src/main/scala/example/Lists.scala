60bdedf90f672eff9c9931873962aba1eb7fd4f1
Chandravadan <s.chandravadan@gmail.com>
2016-06-12 18:27:39 +0530

    Removed return statements
    The last statement in a function is automatically returned

--- example/src/main/scala/example/Lists.scala
+++ example/src/main/scala/example/Lists.scala
@@ -6,68 +6,68 @@ import java.util.NoSuchElementException
 object Lists {
 
   /**
-   * This method computes the sum of all elements in the list xs. There are
-   * multiple techniques that can be used for implementing this method, and
-   * you will learn during the class.
-   *
-   * For this example assignment you can use the following methods in class
-   * `List`:
-   *
-   *  - `xs.isEmpty: Boolean` returns `true` if the list `xs` is empty
-   *  - `xs.head: Int` returns the head element of the list `xs`. If the list
-   *    is empty an exception is thrown
-   *  - `xs.tail: List[Int]` returns the tail of the list `xs`, i.e. the the
-   *    list `xs` without its `head` element
-   *
-   *  ''Hint:'' instead of writing a `for` or `while` loop, think of a recursive
-   *  solution.
-   *
-   * @param xs A list of natural numbers
-   * @return The sum of all elements in `xs`
-   */
-    def sum(xs: List[Int]): Int = {
-    return sumHelper(xs, 0)
+    * This method computes the sum of all elements in the list xs. There are
+    * multiple techniques that can be used for implementing this method, and
+    * you will learn during the class.
+    *
+    * For this example assignment you can use the following methods in class
+    * `List`:
+    *
+    * - `xs.isEmpty: Boolean` returns `true` if the list `xs` is empty
+    * - `xs.head: Int` returns the head element of the list `xs`. If the list
+    * is empty an exception is thrown
+    * - `xs.tail: List[Int]` returns the tail of the list `xs`, i.e. the the
+    * list `xs` without its `head` element
+    *
+    * ''Hint:'' instead of writing a `for` or `while` loop, think of a recursive
+    * solution.
+    *
+    * @param xs A list of natural numbers
+    * @return The sum of all elements in `xs`
+    */
+  def sum(xs: List[Int]): Int = {
+    sumHelper(xs, 0)
   }
 
-  def sumHelper(xs: List[Int], accumulator : Int) : Int = {
+  def sumHelper(xs: List[Int], accumulator: Int): Int = {
     if (xs.isEmpty)
-      return accumulator
+      accumulator
     else
-      return sumHelper(xs.tail, xs.head + accumulator)
+      sumHelper(xs.tail, xs.head + accumulator)
   }
 
 
-  
   /**
-   * This method returns the largest element in a list of integers. If the
-   * list `xs` is empty it throws a `java.util.NoSuchElementException`.
-   *
-   * You can use the same methods of the class `List` as mentioned above.
-   *
-   * ''Hint:'' Again, think of a recursive solution instead of using looping
-   * constructs. You might need to define an auxiliary method.
-   *
-   * @param xs A list of natural numbers
-   * @return The largest element in `xs`
-   * @throws java.util.NoSuchElementException if `xs` is an empty list
-   */
-    def max(xs: List[Int]): Int = {
-    if(xs.isEmpty)
+    * This method returns the largest element in a list of integers. If the
+    * list `xs` is empty it throws a `java.util.NoSuchElementException`.
+    *
+    * You can use the same methods of the class `List` as mentioned above.
+    *
+    * ''Hint:'' Again, think of a recursive solution instead of using looping
+    * constructs. You might need to define an auxiliary method.
+    *
+    * @param xs A list of natural numbers
+    * @return The largest element in `xs`
+    * @throws java.util.NoSuchElementException if `xs` is an empty list
+    */
+  def max(xs: List[Int]): Int = {
+    if (xs.isEmpty)
       throw new NoSuchElementException
     else
-      return maxHelper(xs, xs.head)
+      maxHelper(xs, xs.head)
 
   }
-  def maxHelper(xs: List[Int], maxSoFar : Int): Int ={
+
+  def maxHelper(xs: List[Int], maxSoFar: Int): Int = {
     if (xs.isEmpty)
-      return maxSoFar
-    else{
+      maxSoFar
+    else {
       val thisElement = xs.head
-      if(thisElement > maxSoFar){
-        return maxHelper(xs.tail, thisElement)
+      if (thisElement > maxSoFar) {
+        maxHelper(xs.tail, thisElement)
       }
       else
-        return maxHelper(xs.tail, maxSoFar)
+        maxHelper(xs.tail, maxSoFar)
     }
   }
-  }
+}

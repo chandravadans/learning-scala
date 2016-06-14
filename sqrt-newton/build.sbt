c60878b329b990960d27490c9ab7b5a20e77eb40
Chandravadan <s.chandravadan@gmail.com>
2016-06-14 07:37:28 +0530

    Calculating sqrt of a number using Newton's method

---
+++ sqrt-newton/build.sbt
@@ -0,0 +1,6 @@
+name := "sqrt-newton"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+    
---
+++ sqrt-newton/src/main/scala/SqrtNewton.scala
@@ -0,0 +1,31 @@
+/**
+  * Created by cv on 6/14/16.
+  */
+class SqrtNewton extends App {
+
+  def abs(number: Double) : Double = {
+    if(number < 0)
+      -number
+    else
+      number
+  }
+  def sqrtHelper(guess: Double, number : Double ) : Double = {
+    if(isGoodEnough(guess, number))
+      guess
+    else
+      sqrtHelper(improve(guess, number), number)
+
+  }
+
+  def improve(guess: Double, number: Double) : Double = {
+    (guess + number / guess) / 2
+  }
+
+  def isGoodEnough(guess: Double, number : Double) : Boolean = {
+    abs(guess * guess - number) < 0.001
+  }
+
+  def sqrt(number : Double) : Double = {
+    sqrtHelper(1.0,number)
+  }
+}

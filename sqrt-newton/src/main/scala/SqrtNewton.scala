73c469961ef18be17e022d6a0438e3c59f1d867e
Chandravadan <s.chandravadan@gmail.com>
2016-06-14 08:17:02 +0530

    Used blocks to make the function look cleaner
    Implementation details (helper functions) are now `hidden' inside the function itself.

--- sqrt-newton/src/main/scala/SqrtNewton.scala
+++ sqrt-newton/src/main/scala/SqrtNewton.scala
@@ -3,29 +3,33 @@
   */
 class SqrtNewton extends App {
 
-  def abs(number: Double) : Double = {
-    if(number < 0)
+  def abs(number: Double): Double = {
+    if (number < 0)
       -number
     else
       number
   }
-  def sqrtHelper(guess: Double, number : Double ) : Double = {
-    if(isGoodEnough(guess, number))
-      guess
-    else
-      sqrtHelper(improve(guess, number), number)
 
+  def sqrt(number: Double): Double = {
+
+    def sqrtHelper(guess: Double, number: Double): Double = {
+      if (isGoodEnough(guess, number))
+        guess
+      else
+        sqrtHelper(improve(guess, number), number)
+
+    }
+
+    def improve(guess: Double, number: Double): Double = {
+      (guess + number / guess) / 2
+    }
+
+    def isGoodEnough(guess: Double, number: Double): Boolean = {
+      abs(guess * guess - number) / number < 0.001
+    }
+
+
+    sqrtHelper(1.0, number)
   }
 
-  def improve(guess: Double, number: Double) : Double = {
-    (guess + number / guess) / 2
-  }
-
-  def isGoodEnough(guess: Double, number : Double) : Boolean = {
-    abs(guess * guess - number) / number < 0.001
-  }
-
-  def sqrt(number : Double) : Double = {
-    sqrtHelper(1.0,number)
-  }
 }

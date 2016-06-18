e49690314505d579f59d22b1751d2749b82ad676
Chandravadan <s.chandravadan@gmail.com>
2016-06-18 10:16:03 +0530

    Pascal numbers

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -11,17 +11,40 @@ object Main {
   }
 
   /**
-   * Exercise 1
-   */
-    def pascal(c: Int, r: Int): Int = ???
-  
-  /**
-   * Exercise 2
-   */
-    def balance(chars: List[Char]): Boolean = ???
-  
-  /**
-   * Exercise 3
-   */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    * Exercise 1 : Pascal number
+    * nth pascal number is the same as c(n,r)
+    */
+  def pascal(r: Int, n: Int): Int = {
+    if (n == r || r == 0)
+      1
+    else {
+      factorial(n) / (factorial(n - r) * factorial(r))
+    }
   }
+
+  def factorial(n: Int): Int = {
+
+    def factorialHelper(n: Int, acc: Int): Int = {
+      if (n == 1)
+        acc
+      else
+        factorialHelper((n - 1), acc * n)
+    }
+
+    if (n == 0)
+      1
+    else
+      factorialHelper(n, 1)
+  }
+
+
+  /**
+    * Exercise 2
+    */
+  def balance(chars: List[Char]): Boolean = ???
+
+  /**
+    * Exercise 3
+    */
+  def countChange(money: Int, coins: List[Int]): Int = ???
+}
